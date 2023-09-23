c59b674fef2ba434d5333c6672b7070e662792dd
Ron <lavitr01051977@gmail.com>
2023-09-23 17:26:30 +0200

    fix: code formating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Input from './components/Input'
-import './App.css';
+import Input from "./components/Input";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-     <Input />
+      <Input />
     </div>
   );
 }
